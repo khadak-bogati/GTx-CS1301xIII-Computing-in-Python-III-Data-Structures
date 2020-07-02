28da177a55b0a3edd8ec0dba8642b5add4ee706b
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-07-02 01:20:13 -0500

    Integer by value or reference

---
+++ IntegerByValueorByReference.py
@@ -0,0 +1,11 @@
+# IntegersByValueorByReference
+#ADD one to anInteger
+def addOne(anInteger):
+	anInteger = anInteger + 1
+	print("anInteger: ", anInteger)
+#Create myInteger with the value 5
+myInteger = 5
+print("myInteger befor addone: ", myInteger)
+#call addOne on myInteger
+addOne(myInteger)
+print("myInteger after addOne: ", myInteger)
