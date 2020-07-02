aa96dff31e31a01668a3bf8d0a81a4a1c74669a0
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-07-02 02:05:24 -0500

    Add item by list

---
+++ OthereDataTypeOrlistByValueorReference.py
@@ -0,0 +1,7 @@
+def addItem(aList):
+	aList.append("MyName")
+	print("aList: ", aList)
+aList = ["One", "Two", "Three"]
+print("aList Before addItem: ", aList)
+addItem(aList)
+print("aList After addItem: ", aList)
