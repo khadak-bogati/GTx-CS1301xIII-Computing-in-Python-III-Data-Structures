4e23013df2c3c4f4ff48f276a1f0faffa1943ed2
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-07-07 23:52:55 -0500

    sort atrist

---
+++ Artist.py
@@ -0,0 +1,55 @@
+#We've learned a lot in this chapter. Let's try to use a lot
+#of it for one final exercise.
+#
+#Write a function called sort_artists. sort_artists will
+#take as input a list of tuples. Each tuple will have two
+#items: the first item will be a string holding an artist's
+#name, and the second will be an integer representing their
+#total album sales (in millions).
+#
+#Return a tuple of two lists. The first list in the
+#resulting tuple should be all the artists sorted
+#alphabetically. The second list should be all the revenues
+#sorted in descending numerical order.
+#
+#For example:
+# artists = [("The Beatles", 270.8), ("Elvis Presley", 211.5), ("Michael Jackson", 183.9)]
+# sort_artists(artists) -> (["Elvis Presley", "Michael Jackson", "The Beatles"], [270.8, 211.5, 183.9])
+#
+#Notice that artists is a list of tuples (brackets first,
+#then parentheses), but sort_artists outputs a tuple of
+#lists (parentheses first, then brackets).
+
+
+#Write your function here!
+def sort_artists(myList):
+    result = []
+    value = []
+    for s in myList:
+        for item in s:
+            try:
+                if item.isnumeric:
+                    result.append(item)
+            except:
+                value.append(item)
+    value.sort()
+    value.sort(reverse =True)
+    
+    final = (result, value)
+    return final
+    
+    
+    
+
+#Below are some lines of code that will test your function.
+#You can change the value of the variable(s) to test your
+#function with different inputs.
+#
+#If your function works correctly, this will originally
+#print:
+#(['Elvis Presley', 'Michael Jackson', 'The Beatles'], [270.8, 211.5, 183.9])  
+artists = [("The Beatles", 270.8), ("Elvis Presley", 211.5), ("Michael Jackson", 183.9)]
+print(sort_artists(artists))
+
+
+
