72f0a8aa25cc0cf680d7d11d521d661bf0c385fb
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-07-08 20:07:16 -0500

    Update WritingList.py

--- WritingList.py
+++ WritingList.py
@@ -5,3 +5,19 @@ outputFile = open("OutputFile.txt", "w")
 for name in myList:
 	outputFile.write(name + "\n")
 outputFile.close()
+
+
+
+
+
+==================================================================
+inputFile = open("OutputFile.txt", "r")
+
+myInt1  = int(inputFile.readline())
+myInt2 = int(inputFile.readline())
+myInt3 = int(inputFile.readline())
+
+print("myInt1: ", myInt1)
+print("myInt2: ", myInt2)
+print("myInt3: ", myInt3)
+inputFile.close()
